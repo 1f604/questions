1f6868efe902de9bd32363deeeef44e72de89f8b
1f604 <1f604@gmx.com>
2016-02-24 22:18:40 +0000

    kek

--- programming questions/trivial questions/sumOfIntegers.py
+++ programming questions/trivial questions/sumOfIntegers.py
@@ -1,8 +1,11 @@
 #Sum numbers in an array:
 
-def  sumOfIntegers( arr):
+def  sumOfIntegers( arr): #hmm...
     sum = 0
     for i in arr:
         sum+=i
     return sum
-	
+
+arr = [1,2,3]
+print sum(arr)
+print sumOfIntegers(arr)

0dfa1b54d1c558e5bc4d036941daa51632496987
1f604 <1f604@users.noreply.github.com>
2017-06-10 10:47:13 +0100

    Update two_prophets.py

--- simulations/two_prophets.py
+++ simulations/two_prophets.py
@@ -11,6 +11,8 @@
 #If the world doesn't end then they are both wrong. The chance of that happening is 0.1 * 0.7 = 7%
 #So the chance of the world ending is 27 of (27+7), and that is 27/34 ~=~ 79.4%
 
+#The important thing here is that both prophets made the same guess. 
+
 from random import randint 
 
 A_prob = 90

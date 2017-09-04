53b1b2ffc5c1d1f86a881876563fa9d15f7646dc
1f604 <1f604@users.noreply.github.com>
2017-09-04 13:07:45 +0100

    Create random_map_generator.py

---
+++ simulations/random_map_generator.py
@@ -0,0 +1,27 @@
+#python 2.7
+from hashlib import md5
+import random
+rows = 50
+columns = 50
+mat = [[1 for x in range(columns)] for y in range(rows)]
+seed = random.randint(0,100000)
+def check(r,c):
+    return sum(bytearray(md5(str(seed)+str(r)+' '+str(c)).hexdigest())) % 10 >= 9
+def checkrange(r,c):
+    prob = 0
+    for i in range(r-1,r+2):
+        for j in range(c-1,c+2):
+            if check(i,j):
+                prob += 2
+    if random.randint(0,prob) > 0:
+        return 8
+    return 1
+for r in range(rows):
+    for c in range(columns):
+        if check(r,c):
+            mat[r][c] = 8
+        elif r > 0 and r < rows-1 and c > 0 and c < columns-1:
+            mat[r][c] = checkrange(r,c)
+    
+for r in mat:
+    print r

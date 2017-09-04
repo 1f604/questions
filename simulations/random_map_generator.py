94acbe8d6873e408b157fa0e5b4a2af60ca1dc1f
1f604 <1f604@users.noreply.github.com>
2017-09-04 13:20:03 +0100

    Update random_map_generator.py

--- simulations/random_map_generator.py
+++ simulations/random_map_generator.py
@@ -6,7 +6,7 @@ columns = 50
 mat = [[1 for x in range(columns)] for y in range(rows)]
 seed = random.randint(0,100000)
 def check(r,c):
-    return sum(bytearray(md5(str(seed)+str(r)+' '+str(c)).hexdigest())) % 10 >= 9
+    return sum(bytearray(md5(str(seed)+str(r)+' '+str(c)).hexdigest())) % 12 >= 11
 def checkrange(r,c):
     prob = 0
     for i in range(r-1,r+2):
@@ -14,7 +14,10 @@ def checkrange(r,c):
             if check(i,j):
                 prob += 2
     if random.randint(0,prob) > 0:
-        return 8
+        if prob > 2:
+            return 8
+        else:
+            return 3
     return 1
 for r in range(rows):
     for c in range(columns):
